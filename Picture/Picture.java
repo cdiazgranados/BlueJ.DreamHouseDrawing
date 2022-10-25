aa2bd95837669866e7d7dcf012cc2c93c1fa495c
Carolina <carolina@Zipcoders-MacBook-Pro.local>
2022-10-25 10:04:51 -0400

    First commit

--- Picture/Picture.java
+++ Picture/Picture.java
@@ -1,4 +1,3 @@
-
 /**
  * This class represents a simple picture. You can draw the picture using
  * the draw method. But wait, there's more: being an electronic picture, it
@@ -13,9 +12,17 @@
 public class Picture
 {
     private Square wall;
-    private Square window;
+    private Circle window;
     private Triangle roof;
-    private Circle sun;
+    private Circle shadow;
+    private Circle moon;
+    private Triangle thruster1;
+    private Triangle thruster2;
+    private Triangle flame1;
+    private Triangle flame2;
+    private Triangle flame3;
+    private Triangle flame4;
+    
 
     /**
      * Constructor for objects of class Picture
@@ -30,29 +37,88 @@ public class Picture
      */
     public void draw()
     {
+        thruster1 = new Triangle();
+        thruster1.changeColor("blue");
+        thruster1.changeSize(25, 50);
+        thruster1.moveHorizontal(20);
+        thruster1.moveVertical(215);
+        thruster1.makeVisible();
+        
+        thruster2 = new Triangle();
+        thruster2.changeColor("blue");
+        thruster2.changeSize(25, 50);
+        thruster2.moveHorizontal(100);
+        thruster2.moveVertical(215);
+        thruster2.makeVisible();
+        
+        flame1 = new Triangle();
+        flame1.changeColor("red");
+        flame1.changeSize(15, 25);
+        flame1.moveHorizontal(20);
+        flame1.moveVertical(240);
+        flame1.makeVisible();
+        
+        flame2 = new Triangle();
+        flame2.changeColor("red");
+        flame2.changeSize(15, 25);
+        flame2.moveHorizontal(20);
+        flame2.moveVertical(245);
+        flame2.makeVisible();
+        
+        flame4 = new Triangle();
+        flame4.changeColor("red");
+        flame4.changeSize(15, 25);
+        flame4.moveHorizontal(100);
+        flame4.moveVertical(245);
+        flame4.makeVisible();
+        
+        flame3 = new Triangle();
+        flame3.changeColor("red");
+        flame3.changeSize(15, 25);
+        flame3.moveHorizontal(100);
+        flame3.moveVertical(240);
+        flame3.makeVisible();
+        
+        flame4 = new Triangle();
+        flame4.changeColor("red");
+        flame4.changeSize(15, 25);
+        flame4.moveHorizontal(100);
+        flame4.moveVertical(245);
+        flame4.makeVisible();
+        
         wall = new Square();
-        wall.moveVertical(80);
+        wall.changeColor("white");
+        wall.moveVertical(85);
         wall.changeSize(100);
         wall.makeVisible();
 
-        window = new Square();
-        window.changeColor("black");
-        window.moveHorizontal(20);
-        window.moveVertical(100);
+        window = new Circle();
+        window.changeColor("magenta");
+        window.moveHorizontal(90);
+        window.moveVertical(90);
+        window.changeSize(40);
         window.makeVisible();
-
+        
         roof = new Triangle();
-        roof.changeSize(50, 140);
+        roof.changeSize(50, 100);
+        roof.changeColor("blue");
         roof.moveHorizontal(60);
         roof.moveVertical(70);
         roof.makeVisible();
-
-        sun = new Circle();
-        sun.changeColor("yellow");
-        sun.moveHorizontal(180);
-        sun.moveVertical(-10);
-        sun.changeSize(60);
-        sun.makeVisible();
+        
+        moon = new Circle();
+        moon.changeColor("yellow");
+        moon.moveHorizontal(180);
+        moon.moveVertical(-10);
+        moon.changeSize(60);
+        moon.makeVisible();
+        
+        shadow = new Circle();
+        shadow.changeColor("black");
+        shadow.moveHorizontal(170);
+        shadow.moveVertical(-10);
+        shadow.changeSize(60);
+        shadow.makeVisible();
     }
 
     /**
@@ -62,10 +128,17 @@ public class Picture
     {
         if(wall != null)   // only if it's painted already...
         {
-            wall.changeColor("black");
-            window.changeColor("white");
-            roof.changeColor("black");
-            sun.changeColor("black");
+            wall.changeColor("white");
+            window.changeColor("black");
+            roof.changeColor("white");
+            moon.changeColor("white");
+            shadow.changeColor("black");
+            thruster1.changeColor("white");
+            thruster2.changeColor("white");
+            flame1.changeColor("black");
+            flame2.changeColor("black");
+            flame3.changeColor("black");
+            flame4.changeColor("black");
         }
     }
 
@@ -76,10 +149,17 @@ public class Picture
     {
         if(wall != null)   // only if it's painted already...
         {
-            wall.changeColor("red");
-            window.changeColor("black");
-            roof.changeColor("green");
-            sun.changeColor("yellow");
+            thruster1.changeColor("blue");
+            thruster2.changeColor("blue");
+            wall.changeColor("white");
+            window.changeColor("magenta");
+            roof.changeColor("blue");
+            moon.changeColor("yellow");
+            shadow.changeColor("black");
+            flame1.changeColor("red");
+            flame2.changeColor("red");
+            flame3.changeColor("red");
+            flame4.changeColor("red");
         }
     }
 
